4f21218f43d1a868c0f17b33019168df786854d2
Abhay Faldu <abhayfaldu1922@gmail.com>
2024-10-12 11:25:02 +0530

    chore: changing nav logo text

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <header className="header">
       <Link href="/" className="w-10 h-10 rounded-lg bg-white flex items-center justify-center font-bold shadow-md">
-        <p className="blue-gradient_text">3D</p>
+        <p className="blue-gradient_text">AF</p>
       </Link>
       <nav className="flex text-lg gap-7 font-medium">
         <Link href="/about" >About</Link>
